17ec34b1f54e87691d334770cb48f71094f9fac3
mzerrouk <meidine59@gmail.com>
2022-05-24 11:41:57 +0200

    Update some js things

--- commands/event/index.mjs
+++
@@ -1,7 +0,0 @@
-const output = [
-	"Vous avez trouvé ce que vous êtes venu chercher",
-	"Le prochain event sera un quiz spécialement conçu sur un site web",
-    "Soyez prêt(e)s..."
-];
-
-export { output };
---
+++ commands/notes/index.mjs
@@ -0,0 +1,10 @@
+const output = [
+	"Liste de choses à faire:",
+	"-Finir mes montages tiktok",
+    "-Acheter un pack de 6",
+	"-Garder ma nièce ce week-end",
+	"-2d496d706cc3a96d656e746572206c6120636f6d6d616e6465204556454e54",
+	"-Finir Isaac..."
+];
+
+export { output };
