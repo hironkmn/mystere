60b1c422e97a2c72aed87b9a23086b94bcd2e309
mzerrouk <meidine59@gmail.com>
2022-05-24 12:10:59 +0200

    Commands quoi

--- commands/help/index.mjs
+++ commands/help/index.mjs
@@ -1,6 +1,6 @@
 const output = [
 	"Commandes disponibles:",
-	"help, fallout, brogue, dino, hackerman, matrix, notes, screensaver, cowsay, fire, clear, logout, reboot, quit"
+	"help, fallout, brogue, dino, hackerman, matrix, notes, screensaver, cowsay, fire, clear, logout, reboot, quit, quoi"
 ];
 
 export { output };
