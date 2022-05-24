55d0ad7367344f328193de519feb0ca06f5d3669
mzerrouk <meidine59@gmail.com>
2022-05-24 15:08:15 +0200

    Adding bebou

---
+++ commands/bebou/index.mjs
@@ -0,0 +1,14 @@
+const output = [
+	"-Bebouu",
+    "-Oui bebe",
+    "-Est-ce que tu m'aimes ?",
+    "-Je t'adore",
+    "-Est-ce que tu me GnOk ?",
+    "-Je te GnOk même",
+    "-Est-ce que tu me GnOGnI ??",
+    "-Je te GnOgnI",
+    "-Mouah !",
+    "-MMMMMMMMMMMOUUUUUUUUAAHHHHHHH"
+];
+
+export { output };
